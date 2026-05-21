2d8f28e7b17be7790ba62fde6b484da64027e56e
Tousan1 <stevetamay14@gmail.com>
2026-05-21 13:26:13 -0400

    Added pest violation test analysis

--- analysis.py
+++ analysis.py
@@ -21,3 +21,20 @@ plt.title('Critical health violations by NYC Borough')
 plt.xlabel('Borough')
 plt.ylabel('Number of Critical Violations')
 plt.show()
+
+# Find the top 10 most common violation descriptions in the dataset and print them
+top_violations = df_cleaned['VIOLATION DESCRIPTION'].value_counts().head(10)
+print("\nTop 10 Most Common Violation Descriptions:")
+print(top_violations)
+
+# Create a new column called 'IS_PEST' that is True if 'VIOLATION DESCRIPTION' contains 'mice', 'files', or 'rat' (case-insensitive), and False otherwise
+df_cleaned['IS_PEST'] = df_cleaned['VIOLATION DESCRIPTION'].str.contains('mice|flies|rat', case=False, na=False)
+# Print the total number of pest related violations
+pest_violations_count = df_cleaned['IS_PEST'].sum()
+print(f"\nTotal Pest-Related Violations in NYC: {pest_violations_count}")
+
+# Group by 'BORO' and calculate the percentage of pest-related violations
+pest_violations_by_boro = df_cleaned.groupby('BORO')['IS_PEST'].mean() * 100
+# Print the percentage of pest-related violations for each borough
+print(f"\nPercentage of Pest-Related Violations by Borough:")
+print(pest_violations_by_boro)
